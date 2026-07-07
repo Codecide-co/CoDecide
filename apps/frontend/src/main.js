bf3cb89e17415c71e17e131b62318409af6d46a2
Maria Angelica Muñoz Lopez <marriangelica135@gmail.com>
2026-07-07 08:04:15 -0500

    chore: remove legacy code

--- apps/frontend/src/main.js
+++ apps/frontend/src/main.js
@@ -25,11 +25,9 @@ function showLogin() {
 }
 
 function showRegister() {
-  console.log("Mostrando register");
   authContainer.innerHTML = RegisterView();
 
   initRegisterView();
-  console.log("bendito error")
 
   document.getElementById("go-login").addEventListener("click", (e) => {
     e.preventDefault();
@@ -38,29 +36,3 @@ function showRegister() {
 }
 
 showLogin();
-
-// app.innerHTML = `
-//   <main>
-//     <h1 id="welcome" class="welcome">
-//       Welcome <span id="member-name" class="member-name"></span> to initial CokeDecide
-//     </h1>
-
-//     <div>
-//       <button id="click-me" class="click-me">Click me</button>
-//     </div>
-
-//   </main>
-// `
-
-// document.addEventListener('DOMContentLoaded', () => {
-
-//   const clickMeButton = document.getElementById('click-me');
-//   const memberNameSpan = document.getElementById('member-name');
-//   const members = ['Gustavo', 'Mari', 'Andrea', 'Brandon', 'Carlos', 'Juan'];
-
-//   memberNameSpan.textContent = members[Math.floor(Math.random() * members.length)];
-
-//   clickMeButton.addEventListener('click', () => {
-//     memberNameSpan.textContent = members[Math.floor(Math.random() * members.length)];
-//   });
-// });
