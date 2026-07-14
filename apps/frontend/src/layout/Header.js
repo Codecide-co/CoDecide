42fe738bb203015d7758ee70a9d261e1f341fabb
Zerik-Official <173982082+Zerik-Official@users.noreply.github.com>
2026-07-14 09:16:46 -0500

    fix(header): change hash links to data-link for pushState router

--- apps/frontend/src/layout/Header.js
+++ apps/frontend/src/layout/Header.js
@@ -6,9 +6,9 @@ export function HeaderLanding() {
       </div>
       <nav>
         <ul>
-          <li><a href="/explore-reports" data-link>Explore Reports</a></li>
-          <li><a href="/how-it-works" data-link>How It Works</a></li>
-          <li><a href="/about-us" data-link>About Us</a></li>
+          <li><a href="#explore-reports" data-link>Explore Reports</a></li>
+          <li><a href="#how-it-works" data-link>How It Works</a></li>
+          <li><a href="#about-us" data-link>About Us</a></li>
           <li><a href="/login" data-link>Login</a></li>
           <li><a href="/register" data-link>Register</a></li>
         </ul>
