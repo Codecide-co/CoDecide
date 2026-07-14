12103e6a0d8f1fd3d24be5c3979a8c12204d09f5
Maria Angelica Muñoz Lopez <marriangelica135@gmail.com>
2026-07-14 06:21:26 -0500

    feat(layout): add HeaderLanding and FooterLanding components

---
+++ apps/frontend/src/layout/Header.js
@@ -0,0 +1,18 @@
+export function HeaderLanding() {
+    return `
+    <header class="header-landing">
+      <div>
+        <a id="btn-home" href="#/landing">HOME</a>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#/explore-reports">Explore Reports</a></li>
+          <li><a href="#/how-it-works">How It Works</a></li>
+          <li><a href="#/about-us">About Us</a></li>
+          <li><a href="#/login">Login</a></li>
+          <li><a href="#/register">Register</a></li>
+        </ul>
+      </nav>
+    </header>
+  `;
+}
