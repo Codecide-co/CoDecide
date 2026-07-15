ed3e22c09af664a634a6d9bb0bbc644f39ca7779
Maria Angelica Muñoz Lopez <marriangelica135@gmail.com>
2026-07-15 07:00:32 -0500

    refactor(landing): Add Tailwind CSS elements for responsiveness

--- apps/frontend/src/layout/Header.js
+++ apps/frontend/src/layout/Header.js
@@ -1,18 +1,17 @@
 export function HeaderLanding() {
-    return `
-    <header class="header-landing">
-      <div>
-        <a id="btn-home" href="/" data-link>HOME</a>
-      </div>
-      <nav>
-        <ul>
-          <li><a href="#explore-reports" data-link>Explore Reports</a></li>
-          <li><a href="#how-it-works" data-link>How It Works</a></li>
-          <li><a href="#about-us" data-link>About Us</a></li>
-          <li><a href="/login" data-link>Login</a></li>
-          <li><a href="/register" data-link>Register</a></li>
-        </ul>
-      </nav>
-    </header>
-  `;
+  return `
+  <header class="header-landing flex flex-row justify-between">
+    <div>
+      <a id="btn-home" class="header-logo" href="/" data-link>CoDecide</a>
+    </div>
+    <nav class="hidden md:flex">
+      <ul class="header-nav-list flex flex-row justify-around">
+        <li><a class="header-nav-link" href="#explore-reports" data-link>Explore Reports</a></li>
+        <li><a class="header-nav-link" href="#how-it-works" data-link>How It Works</a></li>
+        <li><a class="header-nav-link" href="#about-us" data-link>About Us</a></li>
+        <li><a class="header-nav-link header-nav-cta" href="/login" data-link>Login</a></li>
+      </ul>
+    </nav>
+  </header>
+`;
 }
