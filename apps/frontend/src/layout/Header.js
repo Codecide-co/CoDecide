eae7a1ec992e8948671fbd053a688eb5f837c672
Maria Angelica Muñoz Lopez <marriangelica135@gmail.com>
2026-07-16 01:36:07 -0500

    chore(home): Add profile link and sidebar toggle to header

--- apps/frontend/src/layout/Header.js
+++ apps/frontend/src/layout/Header.js
@@ -18,9 +18,10 @@ export function HeaderLanding() {
 
 export function HeaderHome() {
   return`
-  <header class="header-home flex flex-row justify-between items-center p-6">
+  <header class="header-home flex flex-row justify-between items-center">
+    <button id="sidebar-toggle" class="sidebar-toggle-btn">☰</button>
     <a id="button-home" class="header-logo" href="/" data-link>CoDecide</a>
-    <a id="button-notification" class="notification-logo" href="/" data-link>🔔</a>
+    <a id="button-profile" href="/profile" data-link class="header-icon"><img src="../../public/user.svg" alt="user">User</a>
   </header>
   `
 }
