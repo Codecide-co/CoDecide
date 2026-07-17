cee6f47e2ae40a492ba9398714d28baa8d56ec40
Maria Angelica Muñoz Lopez <marriangelica135@gmail.com>
2026-07-17 17:35:31 -0500

    refactor(api): export UPLOADS_BASE constant

--- apps/frontend/src/utils/api.js
+++ apps/frontend/src/utils/api.js
@@ -2,6 +2,8 @@ import { getToken } from "@store/auth.store";
 
 const API_URL = "http://localhost:5000/api";
 
+export const UPLOADS_BASE = "http://localhost:5000";
+
 async function request(path, options = {}) {
   const token = getToken();
 
