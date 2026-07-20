6e6a87220ecf8a039b18507147017494b08355ee
Maria Angelica Muñoz Lopez <marriangelica135@gmail.com>
2026-07-20 00:06:57 -0500

    add Reports link to sidebar

--- apps/frontend/src/layouts/Sidebar.js
+++ apps/frontend/src/layouts/Sidebar.js
@@ -37,6 +37,7 @@ export function SidebarHome() {
   const links = [
     { href: "/home", label: "Home", icon: "home.svg" },
     { href: "/reports/create", label: "New Report", icon: "reports.svg" },
+    { href: "/reports", label: "Reports", icon: "reports.svg", icon: "book.svg"},
     { href: "/stats", label: "Statistics", icon: "statistics.svg" },
     { href: "/home/announcements", label: "Comumnicated", icon: "comumnicated.svg" },
     { href: "/profile", label: "My Profile", icon: "user.svg" },
